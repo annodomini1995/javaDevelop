c5eeefe279471bbd702dc8dac347693b670540fd
Arya <Arya@DESKTOP-1OA4J61>
2019-08-30 10:33:02 +0530

    first time commit

---
+++ GitHubSample/src/main/java/mypack/GitHubSample/App.java
@@ -0,0 +1,13 @@
+package mypack.GitHubSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GitHubSample/src/main/java/mypack/GitHubSample/HelloWorld.java
@@ -0,0 +1,13 @@
+package mypack.GitHubSample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("In the main branch");
+
+	}
+
+}
