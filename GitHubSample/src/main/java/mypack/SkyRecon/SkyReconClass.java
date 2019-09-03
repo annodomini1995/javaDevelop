fda95bb76ba29013b4fc5994b875e243294b1e41
Arya <Arya@DESKTOP-1OA4J61>
2019-09-04 01:37:39 +0530

    adding my own function

--- GitHubSample/src/main/java/mypack/SkyRecon/SkyReconClass.java
+++ GitHubSample/src/main/java/mypack/SkyRecon/SkyReconClass.java
@@ -7,30 +7,9 @@ public class SkyReconClass {
 		System.out.println("This class is for SkyRecon PID");
 	}
 
-	void ConflictHere() {
-		
-		String name = "Subrat Tripathi";
-		String name2 = "Akash Ranjan";
-		String name3 = "Priyadarshi Abhishek";
-		String name4 = "Debanjan Paul";
-	}	
-
-	void addingFunctiontoFeature() {
-		System.out.println("Adding this function to the feature branch");
-
+	void myOwnFunction() {
+		System.out.println("I have added my own function");
 	}
-
-	void addingtoMaster() {
-	
-		System.out.println("Added new function to master branch!");
-	}
-	
-void MakeConflict2() {
-	System.out.println("Adding from second contributor");
-		System.out.println("There will be conflict here ...");
-		
-	}
-	
 
 	
 }
